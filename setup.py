d619c09bf1ac3f1f894471b833917aede74e0084
ai-mayurpatil <ai.mayurpatil@gmail.com>
2024-09-09 19:48:04 +0530

    requirements add

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="ai-mayurpatil",
+    author_email="ai.mayurpatil@gmail.com",
+    packages=find_packages()
+)
